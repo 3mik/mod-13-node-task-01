bd36edbdbe76f491422c1b4ac08e1d2b35a2df99
Krzysztof Milek <krzysztof.mile73k@gmail.com>
2018-01-20 16:35:36 +0100

    Complete task

---
+++ app.js
@@ -0,0 +1,29 @@
+process.stdin.setEncoding('utf-8');
+process.stdout.write('App is running...');
+var menu = ('n\nType /menu for menu\nType /info to display information\nType /exit to quit app\n\nWaiting for input...\n');
+process.stdout.write(menu);
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch(instruction) {
+            case '/exit': {
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break 
+            }
+            case '/info': {
+                process.stdout.write(`The operating system platform is ${process.platform}\n`);
+                break
+            }
+            case '/menu': {
+                process.stdout.write(menu);
+                break
+            }
+            default: {
+                process.stderr.write('Wrong instruction!\nTry again...\n');
+                break
+            }
+        }
+    }
+});
